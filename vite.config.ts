bb475263a991610ecc4d74b1a392c007fa182f15
Akash kumaraguru <131514244+akashkumaraguru@users.noreply.github.com>
2025-10-30 10:27:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/bookFinder/", // 👈 REQUIRED for GitHub Pages (use your repo name here)
   server: {
     host: "::",
     port: 8080,
